3ea5d906f8bd00ea2e5980f7bf4e7c5dcc704da6
Jacky <kuoyichun1102@gmail.com>
2016-11-15 11:42:45 +0800

    no message

--- app/user/auth.py
+++ app/user/auth.py
@@ -1,6 +1,6 @@
 from flask import render_template, Blueprint, request
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user, current_user
 from werkzeug.security import generate_password_hash, \
      check_password_hash
 from .form import LoginForm
@@ -12,8 +12,8 @@ db = SQLAlchemy()
 login_manager = LoginManager()
 
 class User(db.Model, UserMixin):
-    # id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, primary_key=True)
+    id = db.Column(db.Integer, unique=True, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
     password = db.Column(db.String(255))
 
     def __init__(self, username, password):
@@ -23,30 +23,35 @@ class User(db.Model, UserMixin):
     def __repr__(self):
         return "%s/%s" % (self.username, self.password)
 
-    def get_id(self):
-        return self.username
+    # def get_id(self):
+    #     return self.username
 
 @auth.route('/signup', methods=['GET', 'POST'])
-def register():
-    form = LoginForm(request.form)
-    if form.validate_on_submit():
-        username = form.username.data
-        password = generate_password_hash(form.password.data)
-        db.session.add(User(username, password))
-        db.session.commit()
-        return 'register successfully'
-    return render_template('signup.html', form=form)
+def signup():
+    if not current_user.is_authenticated:
+        form = LoginForm(request.form)
+        if form.validate_on_submit():
+            username = form.username.data
+            password = generate_password_hash(form.password.data)
+            db.session.add(User(username, password))
+            db.session.commit()
+            return 'register successfully'
+        return render_template('signup.html', form=form)
+    else:
+        return 'hello %s' % current_user.username
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm(request.form)
-    if form.validate_on_submit():
-        user = User.query.get(form.username.data)
-        if user and check_password_hash(user.password, form.password.data):
-            login_user(user)
-            print(user)
-            return 'login successfully'
-    return render_template('login.html', form=form)
+    if not current_user.is_authenticated:
+        form = LoginForm(request.form)
+        if form.validate_on_submit():
+            user = User.query.filter(User.username == form.username.data).first()
+            if user and check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return 'login successfully'
+        return render_template('login.html', form=form)
+    else:
+        return 'hello %s' % current_user.username
 
 
 @auth.route('/logout', methods=['GET', 'POST'])
@@ -56,5 +61,5 @@ def logout():
     return 'logout'
 
 @login_manager.user_loader
-def load_user(username):
-    return User.query.get(username)
+def load_user(userid):
+    return User.query.get(userid)
